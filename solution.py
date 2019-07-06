81687c4ae0c860a350520410a283e963bad31ab0
roman <roman.smirnv@gmail.com>
2019-07-06 20:22:50 +0300

    feat: Initial commitˆ

---
+++ solution.py
@@ -0,0 +1,34 @@
+'''
+
+This source file is an empty solution file inheriting the required spec class for this assignmentself.
+It also demonstrates one way of writing your parses to the required output file
+
+See the spec that this class inherits, for the spec details
+
+'''
+
+from spec import Spec
+from time import sleep
+
+class Submission(Spec):
+
+    def train(self, training_treebank_file='data/heb-ctrees.train'):
+        ''' mock training function, learns nothing '''
+        with open(training_treebank_file, 'r') as train_set:
+            # print(train_set.readline())
+            # sleep(1)
+            for x in train_set:
+                self.parse(x)
+
+    def parse(self, sentence):
+        ''' mock parsing function, returns a constant parse unrelated to the input sentence '''
+        # print(sentence)
+        return '(TOP (S (VP (VB TM)) (NP (NNT MSE) (NP (H H) (NN HLWWIIH))) (yyDOT yyDOT)))'
+    
+    def write_parse(self, sentences, output_treebank_file='output/predicted.txt'):
+        ''' function writing the parse to the output file '''
+        with open(output_treebank_file, 'w') as f:
+            for sentence in sentences:
+                f.write(self.parse(sentence))
+                f.write('\n')
+                
